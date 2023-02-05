9e9e8cc3414e16d3d20938cc599cfbeff544af5f
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-02-05 22:09:48 +0300

    Add the basic structure for a sign up form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
+import Signup from "./Components/signup/Signup";
 
 function App() {
   return (
     <div className="App">
-    Chat application 
-    hello
+      Chat application hello <Signup />
     </div>
   );
 }
--- src/Components/signup/Signup.jsx
+++ src/Components/signup/Signup.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import "./Signup.css";
 
 function Signup() {
   return (
     <div className="signup-container">
-      <img src="" />
+      <div className="img-card">
+        <img
+          src="../assets/images/undraw_share_opinion_re_4qk7.svg"
+          alt="chat"
+        />
+      </div>
       <form className="sign-up">
-<input type="text" />
+        <input type="text" placeholder="Name" name="username" />
+
+        <input type="email" placeholder="Email" name="email" />
+
+        <input type="password" placeholder="Password" name="passowrd" />
+
+        <input
+          type="password"
+          placeholder="Password Confirmation"
+          name="password-confirmation"
+        />
+
+        <input type="file" placeholder="Image" name="image" />
       </form>
     </div>
   );
