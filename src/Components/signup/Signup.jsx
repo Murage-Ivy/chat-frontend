b5e072bbd2c307040942209f50a64e532720d4a3
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-02-07 19:37:29 +0300

    Working on sign up  form styles

--- src/Components/signup/Signup.jsx
+++ src/Components/signup/Signup.jsx
@@ -4,27 +4,36 @@ import "./Signup.css";
 function Signup() {
   return (
     <div className="signup-container">
-      <div className="img-card">
-        <img
-          src="../assets/images/undraw_share_opinion_re_4qk7.svg"
-          alt="chat"
-        />
+      <div className="inner-container">
+        <div className="img-card">
+          <img
+            src="../assets/images/undraw_share_opinion_re_4qk7.svg"
+            alt="chat"
+          />
+        </div>
+        <form className="sign-up">
+          <div className="form-group">
+            <input type="text" name="username" />
+            <label className="floating-label">Name</label>
+          </div>
+          <div className="form-group">
+            <input type="email" name="email" />
+            <label className="floating-label">Email</label>
+          </div>
+
+          <div className="form-group">
+            <input type="password" name="passowrd" />
+            <label className="floating-label">Password</label>
+          </div>
+
+          <div className="form-group">
+            <input type="password" name="password-confirmation" />
+            <label className="floating-label">Password Confirmation</label>
+          </div>
+
+          <input type="file" placeholder="Image" name="image" id="image" />
+        </form>
       </div>
-      <form className="sign-up">
-        <input type="text" placeholder="Name" name="username" />
-
-        <input type="email" placeholder="Email" name="email" />
-
-        <input type="password" placeholder="Password" name="passowrd" />
-
-        <input
-          type="password"
-          placeholder="Password Confirmation"
-          name="password-confirmation"
-        />
-
-        <input type="file" placeholder="Image" name="image" />
-      </form>
     </div>
   );
 }
