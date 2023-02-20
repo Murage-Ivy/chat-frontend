690d167f9c61335a07575c83f35c10899331b33f
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-02-20 21:29:38 +0300

    Style login component

--- src/Components/login/Login.jsx
+++ src/Components/login/Login.jsx
@@ -5,22 +5,30 @@ import "./Login.css";
 
 function Login() {
   return (
-    <div>
+    <div className="login-group">
       <div className="img-login">
-        <img src="" alt="" />
+        <img src="../assets/images/Sign-in-amico.svg" alt="loginpicture" />
       </div>
 
       <form className="login-form">
-        <h2>Welcome Back</h2>
-        <input type="text" placeholder="username" />
-        <FontAwesomeIcon icon={faPerson} className="login-icon" />
-        <br />
-        <input type="password" placeholder="password" />
-        <FontAwesomeIcon icon={faLock} className="login-icon" />
+        <h2 className="welcome-message">Welcome Back</h2>
+
+        <div className=" login-inputs">
+          <FontAwesomeIcon icon={faPerson} className="login-icon" />
+          <input type="text" placeholder="username" />
+        </div>
+
+        <div className="login-inputs">
+          <FontAwesomeIcon icon={faLock} className="login-icon" />
+          <input type="password" placeholder="password" />
+        </div>
 
         <button className="login-btn"> Login</button>
 
-        <p> Don't Have an Account? Sign Up</p>
+        <p className="signup-option">
+          {" "}
+          Don't Have an Account? <span id="sign-up">Sign Up</span>
+        </p>
       </form>
     </div>
   );
